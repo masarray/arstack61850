31067e8a13009401bf0ed1219c76777dfb039d3d
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-09-13 07:17:52 +0700

    perf(ied-sim): instrument signal model responsiveness

--- apps/ied_simulator/src/IedSignalModel.hpp
+++ apps/ied_simulator/src/IedSignalModel.hpp
@@ -4,6 +4,7 @@
 #include "IedFleetController.hpp"
 
 #include <QAbstractListModel>
+#include <QElapsedTimer>
 #include <QHash>
 #include <QTimer>
 #include <QVector>
@@ -20,6 +21,12 @@ class IedSignalModel : public QAbstractListModel {
     Q_PROPERTY(QString logicalNode READ logicalNode WRITE setLogicalNode NOTIFY scopeChanged)
     Q_PROPERTY(QString filterText READ filterText WRITE setFilterText NOTIFY filterTextChanged)
     Q_PROPERTY(int visibleRowCount READ visibleRowCount NOTIFY visibleRowCountChanged)
+    Q_PROPERTY(qulonglong refreshRequestCount READ refreshRequestCount NOTIFY performanceCountersChanged)
+    Q_PROPERTY(qulonglong refreshFlushCount READ refreshFlushCount NOTIFY performanceCountersChanged)
+    Q_PROPERTY(qint64 lastRefreshLatencyMilliseconds READ lastRefreshLatencyMilliseconds NOTIFY performanceCountersChanged)
+    Q_PROPERTY(qint64 maxRefreshLatencyMilliseconds READ maxRefreshLatencyMilliseconds NOTIFY performanceCountersChanged)
+    Q_PROPERTY(qint64 lastRebuildMilliseconds READ lastRebuildMilliseconds NOTIFY performanceCountersChanged)
+    Q_PROPERTY(qint64 maxRebuildMilliseconds READ maxRebuildMilliseconds NOTIFY performanceCountersChanged)
 
 public:
     enum Roles {
@@ -49,18 +56,30 @@ public:
     [[nodiscard]] QString filterText() const;
     void setFilterText(const QString& value);
     [[nodiscard]] int visibleRowCount() const noexcept;
+    [[nodiscard]] quint64 refreshRequestCount() const noexcept { return refreshRequestCount_; }
+    [[nodiscard]] quint64 refreshFlushCount() const noexcept { return refreshFlushCount_; }
+    [[nodiscard]] qint64 lastRefreshLatencyMilliseconds() const noexcept {
+        return lastRefreshLatencyMilliseconds_;
+    }
+    [[nodiscard]] qint64 maxRefreshLatencyMilliseconds() const noexcept {
+        return maxRefreshLatencyMilliseconds_;
+    }
+    [[nodiscard]] qint64 lastRebuildMilliseconds() const noexcept { return lastRebuildMilliseconds_; }
+    [[nodiscard]] qint64 maxRebuildMilliseconds() const noexcept { return maxRebuildMilliseconds_; }
 
     [[nodiscard]] int rowCount(const QModelIndex& parent = QModelIndex{}) const override;
     [[nodiscard]] QVariant data(const QModelIndex& index, int role) const override;
     [[nodiscard]] QHash<int, QByteArray> roleNames() const override;
 
     Q_INVOKABLE void activate(int row);
+    Q_INVOKABLE void resetPerformanceCounters();
 
 signals:
     void backendChanged();
     void scopeChanged();
     void filterTextChanged();
     void visibleRowCountChanged();
+    void performanceCountersChanged();
 
 private:
     enum class RowKind { dataObject, dataAttribute };
@@ -95,11 +114,18 @@ private:
     IedFleetController* backend_{};
     QTimer rebuildTimer_;
     QTimer refreshTimer_;
+    QElapsedTimer refreshLatencyTimer_;
     std::vector<Row> rows_;
     QHash<int, QVector<int>> sourceRows_;
     QString logicalDevice_;
     QString logicalNode_;
     QString filterText_;
+    quint64 refreshRequestCount_{};
+    quint64 refreshFlushCount_{};
+    qint64 lastRefreshLatencyMilliseconds_{};
+    qint64 maxRefreshLatencyMilliseconds_{};
+    qint64 lastRebuildMilliseconds_{};
+    qint64 maxRebuildMilliseconds_{};
     int observedIedIndex_{-1};
     int observedSelectedSourceIndex_{-1};
     int observedSourceCount_{};
