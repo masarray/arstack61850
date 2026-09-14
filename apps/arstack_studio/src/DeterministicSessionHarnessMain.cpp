d4d329e360bf87b49c0bb727af706c76607b97e1
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-09-14 21:17:46 +0700

    S8: add headless harness entry point

---
+++ apps/arstack_studio/src/DeterministicSessionHarnessMain.cpp
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "DeterministicSessionHarness.hpp"
+
+int main(int argc, char* argv[]) {
+    return runDeterministicSessionHarness(argc, argv);
+}
